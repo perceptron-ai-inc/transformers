3c430993c6ca3f9906803b7f9e58b821bdb382ea
Philipp Guevorguian <phil@perceptron.inc>
2025-12-09 12:54:03 +0400

    fix: guard PIL import

--- src/transformers/models/isaac/modular_isaac.py
+++ src/transformers/models/isaac/modular_isaac.py
@@ -104,7 +104,9 @@ if is_torch_available():
 
 
 if is_vision_available():
-    import PIL.Image
+    from PIL.Image import Image
+else:
+    Image = None
 
 
 if is_perceptron_available():
@@ -1611,7 +1613,7 @@ class IsaacProcessor(ProcessorMixin):
     def build_event_stream_simple(
         self,
         text: str,
-        images: Optional[list[PIL.Image.Image]] = None,
+        images: Optional[list[Image]] = None,
     ) -> Stream:
         events = []
         # Process text and images
@@ -1657,7 +1659,7 @@ class IsaacProcessor(ProcessorMixin):
     def __call__(
         self,
         text: Union[str, list[str]],
-        images: Optional[Union[PIL.Image.Image, list[PIL.Image.Image]]] = None,
+        images: Optional[Union[Image, list[Image]]] = None,
         return_tensors: Optional[Union[str, TensorType]] = TensorType.PYTORCH,
         **kwargs,
     ) -> BatchFeature:
@@ -1696,7 +1698,7 @@ class IsaacProcessor(ProcessorMixin):
             texts = text
 
         if images is not None:
-            if isinstance(images, PIL.Image.Image):
+            if isinstance(images, Image):
                 images_list = [images]
             else:
                 images_list = images
